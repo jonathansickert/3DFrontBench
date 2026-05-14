2a5df86149d69aa62a23f94f59540bc9c5f66063
jonathansickert <sickert.jonathan@gmail.com>
2026-05-14 11:17:33 +0200

    improved vlm score prompt

--- src/dlinvc/agents.py
+++ src/dlinvc/agents.py
@@ -14,11 +14,14 @@ class SemanticLabel(Enum):
     DECOR = "decor"
     OTHER = "other"
 
+
 class ObjectBoundingBox(BaseModel):
     id: str = Field("Unique identifier for the object")
     label: SemanticLabel = Field("Semantic label of the object")
     extents: list[float] = Field("Bounding box lenghts along axes (x, y, z)")
-    center: list[float] = Field("Center coordinates of the bounding box in world space (x, y, z) in right-handed Y-up coordinate system")
+    center: list[float] = Field(
+        "Center coordinates of the bounding box in world space (x, y, z) in right-handed Y-up coordinate system"
+    )
     orientation: list[float] = Field(
         "Orientation of the bounding box as a quaternion (x, y, z, w) in right-handed Y-up coordinate system"
     )
@@ -58,31 +61,31 @@ class CoarseGeometryAgent:
 
         coarse_scene = response.parsed
         return coarse_scene
-    
 
-class CoarseGeometryInspectorAgent():
+
+class CoarseGeometryInspectorAgent:
     def __init__(self, api_key: str | None = None):
-            api_key = api_key or os.getenv("GEMINI_API_KEY")
-            if not api_key:
-                raise ValueError("No API key found.")
+        api_key = api_key or os.getenv("GEMINI_API_KEY")
+        if not api_key:
+            raise ValueError("No API key found.")
 
-            self.client = genai.Client(api_key=api_key)
+        self.client = genai.Client(api_key=api_key)
 
-    def generate_score(self, target_image_path: str, rendering_image_path: str, prompt: str, scene: CoarseScene = None) -> VLMSceneScore:
+    def generate_score(
+        self, target_image_path: str, rendering_image_path: str, prompt: str, scene: CoarseScene = None
+    ) -> VLMSceneScore:
         target_image = Image.open(target_image_path)
         rendering_image = Image.open(rendering_image_path)
 
         response = self.client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.1-flash-lite",
             contents=[prompt, target_image, rendering_image],
-            config={
-                "response_mime_type": "application/json",
-                "response_schema": VLMSceneScore
-            }
+            config={"response_mime_type": "application/json", "response_schema": VLMSceneScore},
         )
 
         vlm_score = response.parsed
         return vlm_score
+
 
 if __name__ == "__main__":
     agent = CoarseGeometryAgent()
