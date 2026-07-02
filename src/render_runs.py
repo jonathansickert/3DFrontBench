02476e8aa3bdba6455fe837e844331ebc61f2d00
jonathansickert <sickert.jonathan@gmail.com>
2026-07-02 12:03:24 +0200

    updated render runs script

--- src/render_runs.py
+++ src/render_runs.py
@@ -1,24 +1,27 @@
 from pathlib import Path
 import os
 import subprocess
+import re
 
-run_dir = Path("/Users/jonathansickert/Downloads/runs_kimi2.7")
+run_dir = Path("/Users/jonathansickert/Downloads/runs_gemma31b_no-ver")
 dataset_dir = Path("/Users/jonathansickert/git/3DFrontBench/dataset")
 
 BLENDER_CMD = os.getenv("BLENDER_PATH")
 RENDER_SCRIPT = "/Users/jonathansickert/git/3DFrontBench/src/blender/render_vlm_output.py"
 
-OUTPUT = Path("./runs_kimi/")
+OUTPUT = Path("./runs_gemma_no_ver")
 OUTPUT.mkdir(exist_ok=True, parents=True)
+
 
 
 for run in run_dir.iterdir():
     if not run.is_dir():
         continue
 
-    referecence_image = dataset_dir / run.name / "color.png"
-    cam_path = dataset_dir / run.name / "camera.json"
-    outpath = OUTPUT / run.name
+    run_name = re.sub(r'_\d{8}_\d{6}.*$', '', run.name)
+    referecence_image = dataset_dir / run_name / "color.png"
+    cam_path = dataset_dir / run_name / "camera.json"
+    outpath = OUTPUT / run_name
     outpath.mkdir(exist_ok=True, parents=True)
 
     for step in run.iterdir():
