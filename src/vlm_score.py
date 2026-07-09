0a1c549c84303aed6cde7d91346d79e8503811af
jonathansickert <sickert.jonathan@gmail.com>
2026-07-09 15:02:36 +0200

    updated vlm scroe

--- src/vlm_score.py
+++ src/vlm_score.py
@@ -15,6 +15,25 @@ class VLMSceneScore(BaseModel):
     object_orientation: int
     reasoning: str
 
+class VLMPlacementScore(BaseModel):
+    object_placement: int
+    reasoning: str
+    missing_info: str
+
+class VLMScaleScore(BaseModel):
+    object_scale: int
+    reasoning: str
+    missing_info: str
+
+class VLMOrientationScore(BaseModel):
+    object_orientation: int
+    reasoning: str
+    missing_info: str
+
+class VLMCountScore(BaseModel):
+    object_count: int
+    reasoning: str
+    missing_info: str
 
 def _image_to_data_url(image: Image.Image) -> str:
     buffer = io.BytesIO()
@@ -24,14 +43,19 @@ def _image_to_data_url(image: Image.Image) -> str:
 
 
 class VLMScoreAgent:
-    def __init__(self, api_key: str | None = None):
-        api_key = api_key or os.getenv("OPENROUTER_API_KEY")
+    def __init__(self, api_key: str | None = None, base_url: str | None = None):
+        api_key = api_key or os.getenv("GEMINI_API_KEY")
+        base_url = base_url or os.getenv("GEMINI_BASE_URL")
+
         if not api_key:
             raise ValueError("No API key found.")
+        
+        if not base_url:
+            raise ValueError("No base URL key found.")
 
         self.client = openai.OpenAI(
             api_key=api_key,
-            base_url="https://openrouter.ai/api/v1",
+            base_url=base_url,
         )
 
     def generate_score(
@@ -39,6 +63,7 @@ class VLMScoreAgent:
         target_image: Image.Image,
         rendering_image: Image.Image,
         prompt: str,
+        response_format: BaseModel,
         max_retries: int = 3,
     ) -> VLMSceneScore:
         for attempt in range(max_retries):
@@ -64,7 +89,7 @@ class VLMScoreAgent:
                             ],
                         },
                     ],
-                    response_format=VLMSceneScore,
+                    response_format=response_format,
                     max_tokens=1024,
 
                 )
@@ -77,24 +102,35 @@ class VLMScoreAgent:
 
 
 
-def compute_vlm_score(target_path: str, render_path: str, runs: int = 1) -> list[dict[str, float]]:
+def compute_vlm_score(target_path: str, render_path: str, score_type: str) -> dict[str, float]:
     vlm_agent = VLMScoreAgent()
-    with open("/Users/jonathansickert/git/3DFrontBench/prompts/vlm_score_prompt.txt") as f:
-        prompt = f.read()
+
+    if score_type == "count":
+        with open("/Users/jonathansickert/git/3DFrontBench/prompts/vlm_count_score_prompt.txt") as f:
+            prompt = f.read()
+        response_format = VLMCountScore
+    elif score_type == "placement":
+        with open("/Users/jonathansickert/git/3DFrontBench/prompts/vlm_placement_prompt.txt") as f:
+            prompt = f.read()
+        response_format = VLMPlacementScore
+    elif score_type == "orientation":
+        with open("/Users/jonathansickert/git/3DFrontBench/prompts/vlm_orientation_score_prompt.txt") as f:
+            prompt = f.read()
+        response_format = VLMOrientationScore
+    elif score_type == "scale":
+        with open("/Users/jonathansickert/git/3DFrontBench/prompts/vlm_scale_score_prompt.txt") as f:
+            prompt = f.read()
+        response_format = VLMScaleScore
+
 
     target_img = Image.open(target_path)
     render_img = Image.open(render_path)
 
-    results = []
-    for _ in range(runs):
-        result = vlm_agent.generate_score(
-            target_image=target_img,
-            rendering_image=render_img,
-            prompt=prompt,
-        )
+    result = vlm_agent.generate_score(
+        target_image=target_img,
+        rendering_image=render_img,
+        response_format=response_format,
+        prompt=prompt,
+    )
 
-        results.append(
-            result.model_dump_json()
-        )
-
-    return results
+    return result.model_dump()
